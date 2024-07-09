5cf5ffabc31d7aa168cf9e2e03033a2221c176cb
Arjonnill07 <78968986+arjonnill07@users.noreply.github.com>
2024-07-09 20:18:02 +0600

    added a new get line fuction ,used string concatanation and value in between range

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+MAX_LINES = 3 #Global Constant
+
 def deposite():
     while True:
         amount = input("what would you like to deposite? $")
@@ -12,4 +14,25 @@ def deposite():
             print("Please enter a number")
     return amount 
 
-deposite() #function call
+def get_no_of_lines():
+    while True:
+        lines = input("How many lines would you like to play? (1-" + str(MAX_LINES) + "):? ") #string_conversion and concatanation
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES: #checking if value is inbetween
+                break
+            else:
+                print("Please enter a valid number of lines")
+        else:
+            print("Please enter a number")
+    return lines
+
+        
+def main():
+    balance = deposite() #function call
+    lines = get_no_of_lines()
+    print(balance,lines)
+
+main()
+    
+    
